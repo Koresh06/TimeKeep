501ea6c751638bf5ccef882c6829e52b11ae463a
Koresh06 <korets-24@mail.ru>
2025-01-19 19:25:06 +0300

    addad template home

---
+++ src/api/v1/user/main_router.py
@@ -0,0 +1,38 @@
+from typing import Annotated
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.conf_static import templates
+from src.api.v1.day_off.dependencies import count_notifications_day_offs
+from src.core.session import get_async_session
+from src.api.v1.auth.dependencies import get_current_user
+from src.middlewares.notification.dependencies import get_unread_notifications_count_user
+from src.models import User
+
+
+router = APIRouter(
+    prefix="/home"
+)
+
+@router.get("/", response_class=HTMLResponse)
+async def home_page(
+    request: Request, 
+    session: Annotated[
+        AsyncSession,
+        Depends(get_async_session),
+    ],
+    current_user: User = Depends(get_current_user),
+    count_day_offs: int = Depends(count_notifications_day_offs),
+    notifications_count_user: int = Depends(get_unread_notifications_count_user),
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={
+            "current_user": current_user,
+            "count_day_offs": count_day_offs,
+            "notifications_count_user": notifications_count_user
+        },
+    )
+    
