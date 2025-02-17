eaecb923031cb1de914d766638bfb949de494574
Koresh06 <korets-24@mail.ru>
2025-02-17 21:16:40 +0300

    change session

--- src/core/session.py
+++ src/core/session.py
@@ -4,27 +4,23 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
 )
-
-
+from sqlalchemy.pool import AsyncAdaptedQueuePool
 from src.core.config import settings
-
-
 
 engine = create_async_engine(
     url=settings.db.construct_sqlalchemy_url(),
     query_cache_size=1200,
-    pool_size=20,
-    max_overflow=200,
+    poolclass=AsyncAdaptedQueuePool,
+    pool_recycle=1800, 
+    pool_pre_ping=True, 
     future=True,
     echo=False,
 )
-
 
 async_session_maker = async_sessionmaker(
     bind=engine,
     expire_on_commit=False
 )
-
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
@@ -35,40 +31,3 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
             raise
         finally:
             await session.close()
-
-
-# class Database:
-#     def __init__(self, db_url: str) -> None:
-#         self.db_url = db_url
-#         self._engine: AsyncEngine | None = create_async_engine(url=db_url, echo=True)
-#         self._sessionmaker: async_sessionmaker[AsyncSession] | None = (
-#             async_sessionmaker(
-#                 autocommit=False,
-#                 autoflush=False,
-#                 bind=self._engine,
-#             )
-#         )
-
-#     async def close(self) -> None:
-#         if self._engine is None:
-#             raise DatabaseError(message="DatabaseSessionManager is not initialized")
-#         await self._engine.dispose()
-
-#         self._engine = None
-#         self._sessionmaker = None
-
-#     @contextlib.asynccontextmanager
-#     async def session(self) -> AsyncIterator[AsyncSession]:
-#         if self._sessionmaker is None:
-#             raise DatabaseError(message="DatabaseSessionManager is not initialized")
-
-#         session = self._sessionmaker()
-#         try:
-#             yield session
-#         except Exception as err:
-#             await session.rollback()
-#             raise DatabaseError(
-#                 message=f"An error occurred during the session {err}"
-#             ) from err
-#         finally:
-#             await session.close()
